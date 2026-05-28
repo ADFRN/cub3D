9aed46934a121d7a0c1709899029de4eb0a5580b
ttiprez <tipr.tuan@gmail.com>
2026-05-28 15:34:03 +0200

    Refactor de t_game.c

--- src/structures/t_game.c
+++ src/structures/t_game.c
@@ -6,7 +6,7 @@
 /*   By: ttiprez <ttiprez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 19:21:45 by ttiprez           #+#    #+#             */
-/*   Updated: 2026/05/27 14:35:12 by ttiprez          ###   ########.fr       */
+/*   Updated: 2026/05/28 15:33:37 by ttiprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ t_game	t_game_new(char *map_path)
 	game.map = t_map_new(&game, map_path);
 	if (!is_valid_texture_path(&game) || !is_valid_texture(&game))
 		exit(1);
-	game.player = t_player_new(game);
 	game.ray = t_ray_new();
 	game.minimap = t_minimap_new(game.map, game.ray);
+	game.player = t_player_new(game);
 	game.keys = t_keys_new();
 	game.data = t_data_new(game.mlx);
 	init_textures(&game);
